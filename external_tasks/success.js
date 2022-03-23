83a97fd0365d20e6eb6bb6952455f7d9f4faaea4
Wisnu Manupraba <wisnu@javan.co.id>
2022-03-23 20:24:16 +0700

    completed api

--- external_tasks/success.js
+++ external_tasks/success.js
@@ -1,9 +1,15 @@
 const { Client, logger } = require("camunda-external-task-client-js");
 
+const request = require('request');
+
+
+
 // configuration for the Client:
 //  - 'baseUrl': url to the Process Engine
 //  - 'logger': utility to automatically log important events
-const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+const REACT_APP_CAMUNDA_API = "http://localhost:8080/engine-rest";
+
+const config = { baseUrl: REACT_APP_CAMUNDA_API, use: logger };
 
 // create a Client instance with custom configuration
 const client = new Client(config);
@@ -12,47 +18,36 @@ const client = new Client(config);
 client.subscribe("approved", async function({ task, taskService }) {
   // Put your business logic
   // complete the task
-  console.log("Hello world");
-  console.log(task.id + " was here");
-  
-  fetch(process.env.REACT_APP_CAMUNDA_API + "/process-instance/" + this.props.id + "/variables", {
-    method: "GET",
-    headers: new Headers({
-      'Content-Type':'application/json'
-    }),
-    }) .then((res) => res.json())
-    .then((json) => {
-        fetch(process.env.REACT_APP_CAMUNDA_API + "/process-definition/key/approved_expense/start", {
-            method: "POST",
-            headers: new Headers({
-              'Content-Type':'application/json'
-            }),
-            body: JSON.stringify(
-              {
-                "variables": {
-                  "name" : {
-                      "value" : json.name,
-                      "type": "String"
-                  },
-                  "amount" : {
-                    "value" : this.state.amount,
-                    "type": "Double"
-                  },
-                  "description" : {
-                      "value" : this.state.description,
-                      "type": "String"
-                  }
-                },
-               "businessKey" : this.state.name
-              }
-            )
-          }).then((json)=>{
-              alert("Submitted")
-          });
-    }
-    )
+  request(REACT_APP_CAMUNDA_API + "/process-instance/" + task.processInstanceId + "/variables", { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
 
-  
-
+    request.post({
+      headers : {'Content-Type':'application/json'},
+      url : REACT_APP_CAMUNDA_API + "/process-definition/key/approved_expense/start",
+      body :JSON.stringify(
+                    {
+                      "variables": {
+                        "name" : {
+                            "value" : body.name.value,
+                            "type": "String"
+                        },
+                        "amount" : {
+                          "value" : body.amount.value,
+                          "type": "Double"
+                        },
+                        "description" : {
+                            "value" : body.description.value,
+                            "type": "String"
+                        }
+                      },
+                     "businessKey" : body.name.value
+                    }
+                  )
+    }, function(error, response, body){
+      console.log(body);
+    });
+    
+  });
   await taskService.complete(task);
+  console.log("DONE");
 });
